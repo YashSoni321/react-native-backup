f6a0f94c2bc539d4b2db2df16e95336ec6eee551
Yash-solvitine <yashsoni@heyneo.so>
2025-08-13 08:05:40 +0530

    changes pushed

--- Modules/Common/locationHelper.js
+++ Modules/Common/locationHelper.js
@@ -18,99 +18,51 @@ export const requestLocationPermission = async () => {
   }
 };
 
-// export const getUserLocation = (showModalCallback = null) => {
-//   return new Promise(async (resolve, reject) => {
-//     const hasPermission = await requestLocationPermission();
-
-//     if (!hasPermission) {
-//       // if (showModalCallback) {
-//       //   showModalCallback(
-//       //     'Permission Denied',
-//       //     'Location access is required to find nearby services.',
-//       //     'error',
-//       //   );
-//       // }
-//       return reject(new Error('Location permission denied'));
-//     } else {
-//       if (showModalCallback) {
-//         // showModalCallback(
-//         //   'Location Permission',
-//         //   'Location permission is required for the app to function properly!',
-//         //   'info',
-//         //   () => {
-//         //     if (Platform.OS === 'android') {
-//         //       Linking.openSettings(); // Opens App Settings
-//         //     } else {
-//         //       Linking.openURL('App-Prefs:root=Privacy&path=LOCATION'); // iOS (but limited)
-//         //     }
-//         //   },
-//         // );
-//       }
-//     }
-
-//     const timeoutId = setTimeout(() => {
-//       reject(new Error('Location request timed out'));
-//     }, 15000); // 15 seconds timeout
-
-//     Geolocation.getCurrentPosition(
-//       position => {
-//         // clearTimeout(timeoutId);
-//         resolve({
-//           latitude: position.coords.latitude,
-//           longitude: position.coords.longitude,
-//         });
-//       },
-//       error => {
-//         // clearTimeout(timeoutId);
-
-//         let message = 'Unable to fetch location.';
-//         switch (error.code) {
-//           case 1:
-//             message = 'Permission denied.';
-//             break;
-//           case 2:
-//             message = 'Location unavailable.';
-//             break;
-//           case 3:
-//             message = 'Location timeout.';
-//             break;
-//         }
-
-//         // if (showModalCallback) {
-//         //   showModalCallback('Location Error', message, 'error');
-//         // }
-//         // reject(new Error(message));
-//       },
-//       {
-//         enableHighAccuracy: true,
-//         timeout: 15000,
-//         maximumAge: 300000,
-//         distanceFilter: 10,
-//       },
-//     );
-//   });
-// };
-
-
-// import { Platform, Linking } from 'react-native';
-// import Geolocation from '@react-native-community/geolocation';
-
-export const getUserLocation = async (showModalCallback = null) => {
+export const getUserLocation = (showModalCallback = null) => {
   return new Promise(async (resolve, reject) => {
     const hasPermission = await requestLocationPermission();
 
     if (!hasPermission) {
+      // if (showModalCallback) {
+      //   showModalCallback(
+      //     'Permission Denied',
+      //     'Location access is required to find nearby services.',
+      //     'error',
+      //   );
+      // }
       return reject(new Error('Location permission denied'));
+    } else {
+      if (showModalCallback) {
+        // showModalCallback(
+        //   'Location Permission',
+        //   'Location permission is required for the app to function properly!',
+        //   'info',
+        //   () => {
+        //     if (Platform.OS === 'android') {
+        //       Linking.openSettings(); // Opens App Settings
+        //     } else {
+        //       Linking.openURL('App-Prefs:root=Privacy&path=LOCATION'); // iOS (but limited)
+        //     }
+        //   },
+        // );
+      }
     }
+
+    const timeoutId = setTimeout(() => {
+      reject(new Error('Location request timed out'));
+    }, 15000); // 15 seconds timeout
 
     Geolocation.getCurrentPosition(
       position => {
+        // clearTimeout(timeoutId);
         resolve({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         });
       },
       error => {
+        // clearTimeout(timeoutId);
+
         let message = 'Unable to fetch location.';
         switch (error.code) {
           case 1:
@@ -120,15 +72,19 @@ export const getUserLocation = async (showModalCallback = null) => {
             message = 'Location unavailable.';
             break;
           case 3:
-            message = 'Location request timed out.';
+            message = 'Location timeout.';
             break;
         }
-        reject(new Error(message));
+
+        // if (showModalCallback) {
+        //   showModalCallback('Location Error', message, 'error');
+        // }
+        // reject(new Error(message));
       },
       {
         enableHighAccuracy: true,
-        timeout: 20000, // 20 sec
-        maximumAge: 10000,
+        timeout: 15000,
+        maximumAge: 300000,
         distanceFilter: 10,
       },
     );
